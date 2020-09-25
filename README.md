dc835f45bbe12115ad43a0375ea7c7f0160a9f03
Alexander Levin <sanchezable@yandex.ru>
2020-09-25 19:33:55 +0300

    upd: github sync;

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Source Code Classification
-This is a repo of NL2ML-project of the Laboratory of Methods for Big Data Analysis at Higher School of Economics (HSE LAMBDA).\
-The repo is a mirror of HSE LAMBDA GitLab - https://gitlab.com/lambda-hse/nl2ml \
-The project page - https://www.notion.so/NL2ML-Corpus-1ed964c08eb049b383c73b9728c3a231
+__This is a repo of the Natural Language to Machine Learning (NL2ML) project of the Laboratory of Methods for Big Data Analysis at Higher School of Economics (HSE LAMBDA).__
 
-## Project Goals:
+The project's official __repo__ is stored on GitLab (HSE LAMBDA repository) - https://gitlab.com/lambda-hse/nl2ml \
+The project's __full description__ is stored on Notion - https://www.notion.so/NL2ML-Corpus-1ed964c08eb049b383c73b9728c3a231 \
+The project's __experiments__ are stored on DAGsHub - https://dagshub.com/levin/source_code_classification
 
-The current short-term goal is to build a model that will be able to classify a source code chunk and to specify where the detected class is exactly in the chunk (tag segmentation).
+## Project Goals
+### Short-Term Goal
+To build a model classifying a source code chunk and to specify where the detected class is exactly in the chunk (tag segmentation).
+### Long-Term Goal
+To build a model generating code by getting a short raw english task in as an input.
 
-The main goal is to build a model that will be able to generate code getting a text of the task in english as an input.
+## Repository Description
+This repository contains instruments which the project's team has been using to label source code chunks with Knowledge Graph vertices and to train models to recognize these vertices in future. By the Knowledge Graph vertices we mean an elementary part of ML-pipeline. The current latest version of the Knowledge Graph contains the following high-level vertices: `['import', 'data_import', 'data_export', 'preprocessing', 'visualization', 'model', 'deep_learning_model', 'train' 'predict']`.
+
+## Data Download
+To download the project data and models:
+1. Clone this repository
+2. Install DVC from https://dvc.org/doc/install
+3. Do `dvc pull data` or `dvc pull data`. Note: if you are failing on `dvc pull [folder_to_pull]`, try `dvc pull [folder_to_pull] --jobs 1`
 
 ## Contents:
+The instruments which we have been using to reach the project goals are: notebooks parsing from Kaggle API and Github API, data preparation, regex-labellig, training models, validation models, model weights/coefficients analysis, errors analysis, synonyms analysis.
+
 __nl2ml_notebook_parser.py__ - a script for parsing Kaggle notebooks and process them to JSON/CSV/Pandas.
 
 __bert_distances.ipynb__ - a notebook with BERT expiremints concerning sense of distance between BERT embeddings where input tokens were tokenized source code chunks.
@@ -18,7 +31,7 @@ __bert_classifier.ipynb__ - a notebook with preprocessing and training BERT-pipe
 
 __regex.ipynb__ - a notebook with creating labels for code chunks with regex
 
-__logreg_classifier.ipynb.ipynb__ - a notebook with training logistic regression model on the regex labels with tf-idf and analyzing the outputs
+__logreg_classifier.ipynb__ - a notebook with training logistic regression model on the regex labels with tf-idf and analyzing the outputs
 
 __Comments vs commented code.ipynb__ - a notebook with a model distinguishing NL-comments from commented source code
 
--- svm_train.py
+++ svm_train.py
@@ -10,12 +10,13 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import BaggingClassifier
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multioutput import MultiOutputRegressor
 
 import dagshub
 
 def load_code_blocks(DATASET_PATH, CODE_COLUMN):
     df = pd.read_csv(DATASET_PATH, encoding='utf-8', comment='#', sep=',')#, quoting=csv.QUOTE_NONE, error_bad_lines=False)#, sep=','
-    print(df.head())
+    df.dropna(axis=0, inplace=True)
     code_blocks = df[CODE_COLUMN]
     # test_size = 0.1
     # test_rows = round(df.shape[0]*test_size)
@@ -34,7 +35,7 @@ def tfidf_fit_transform(code_blocks, params, TFIDF_DIR):
 
 def SVM_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params):
     code_blocks_tfidf = tfidf_fit_transform(code_blocks, tfidf_params, TFIDF_DIR)
-    X_train, X_test, y_train, y_test = train_test_split(code_blocks_tfidf, df[TAG_TO_PREDICT], test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(code_blocks_tfidf, df[TAGS_TO_PREDICT], test_size=0.3)
     # grid = {"C": [100]}
     # cv = KFold(n_splits=2, shuffle=True, random_state=241)
     model = SVC(kernel="linear", random_state=241)
@@ -60,13 +61,42 @@ def SVM_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params):
     print(metrics)
     return metrics
 
+def SVM_multioutput_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params):
+    code_blocks_tfidf = tfidf_fit_transform(code_blocks, tfidf_params, TFIDF_DIR)
+    X_train, X_test, y_train, y_test = train_test_split(code_blocks_tfidf, df[TAGS_TO_PREDICT], test_size=0.3)
+    # grid = {"C": [100]}
+    # cv = KFold(n_splits=2, shuffle=True, random_state=241)
+    model = SVC(kernel="linear", random_state=241)
+    # gs = GridSearchCV(model, grid, scoring="accuracy", cv=cv, verbose=1, n_jobs=-1)
+    # gs.fit(X_train[:25000], y_train.ravel()[:25000])
+    # C = gs.best_params_.get('C')
+    # model = SVC(**SVM_params)
+    print("Train SVM params:", model.get_params())
+    n_estimators = 10
+    clf = MultiOutputRegressor(BaggingClassifier(model, max_samples=1.0 / n_estimators, n_estimators=n_estimators))
+    # clf = model
+    print("starting training..")
+    clf.fit(X_train, y_train)
+    print("saving the model")
+    pickle.dump(clf, open(MODEL_DIR, 'wb'))
+    print("predicting on the test..")
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    # confus_matrix = confusion_matrix(model, X_test, y_test)
+    metrics = {'test_accuracy': accuracy
+            , 'test_f1_score': f1}
+    print(metrics)
+    return metrics
+
 if __name__ == '__main__':
-    GRAPH_VERSION = 3.1
+    GRAPH_VERSION = 5
     DATASET_PATH = './data/code_blocks_regex_graph_v{}.csv'.format(GRAPH_VERSION)
     MODEL_DIR = './models/svm_regex_graph_v{}.sav'.format(GRAPH_VERSION)
     TFIDF_DIR = './models/tfidf_svm_graph_v{}.pickle'.format(GRAPH_VERSION)
     CODE_COLUMN = 'code_block'
-    TAG_TO_PREDICT = 'preprocessing'
+    TAGS_TO_PREDICT = ['import', 'data_import', 'data_export', 'preprocessing',
+                        'visualization', 'model', 'deep_learning_model', 'train', 'predict']
     SCRIPT_DIR = __file__
     
     df, code_blocks = load_code_blocks(DATASET_PATH, CODE_COLUMN)
@@ -80,13 +110,13 @@ if __name__ == '__main__':
             , 'random_state':241}
     data_meta = {'DATASET_PATH': DATASET_PATH
                 ,'nrows': nrows
-                ,'label': TAG_TO_PREDICT
+                ,'label': TAGS_TO_PREDICT
                 ,'model': MODEL_DIR
-                ,'source': SCRIPT_DIR}
+                ,'script_dir': SCRIPT_DIR}
 
     with dagshub.dagshub_logger() as logger:
         print("evaluating..")
-        metrics = SVM_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params)
+        metrics = SVM_multioutput_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params)
         print("saving the results..")
         logger.log_hyperparams(data_meta)
         logger.log_hyperparams(tfidf_params)
