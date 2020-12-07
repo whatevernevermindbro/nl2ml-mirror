e2ee20cd4c01209633d87314dc60d1d6d2b9e901
Aniezka <aniezka.sherbakova@gmail.com>
2020-12-08 00:52:35 +0300

    First run

--- svm_train.py
+++ svm_train.py
@@ -14,8 +14,10 @@ from sklearn.multioutput import MultiOutputRegressor
 
 import dagshub
 
+
 def load_code_blocks(DATASET_PATH, CODE_COLUMN):
-    df = pd.read_csv(DATASET_PATH, encoding='utf-8', comment='#', sep=',')#, quoting=csv.QUOTE_NONE, error_bad_lines=False)#, sep=','
+    df = pd.read_csv(DATASET_PATH, encoding='utf-8', comment='#',
+                     sep=',')  # , quoting=csv.QUOTE_NONE, error_bad_lines=False)#, sep=','
     df.dropna(axis=0, inplace=True)
     code_blocks = df[CODE_COLUMN]
     # test_size = 0.1
@@ -25,6 +27,7 @@ def load_code_blocks(DATASET_PATH, CODE_COLUMN):
     # test_code_blocks = df[CODE_COLUMN][train_rows:]
     return df, code_blocks
 
+
 def tfidf_fit_transform(code_blocks, params, TFIDF_DIR):
     vectorizer = TfidfVectorizer(**params)
     tfidf = vectorizer.fit(code_blocks)
@@ -32,6 +35,7 @@ def tfidf_fit_transform(code_blocks, params, TFIDF_DIR):
     print('TF-IDF model has been saved')
     code_blocks_tfidf = tfidf.transform(code_blocks)
     return code_blocks_tfidf
+
 
 def SVM_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params):
     code_blocks_tfidf = tfidf_fit_transform(code_blocks, tfidf_params, TFIDF_DIR)
@@ -57,9 +61,10 @@ def SVM_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params):
     f1 = f1_score(y_test, y_pred, average='weighted')
     # confus_matrix = confusion_matrix(model, X_test, y_test)
     metrics = {'test_accuracy': accuracy
-            , 'test_f1_score': f1}
+        , 'test_f1_score': f1}
     print(metrics)
     return metrics
+
 
 def SVM_multioutput_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_params):
     code_blocks_tfidf = tfidf_fit_transform(code_blocks, tfidf_params, TFIDF_DIR)
@@ -85,34 +90,35 @@ def SVM_multioutput_evaluate(df, code_blocks, tfidf_params, TFIDF_DIR, SVM_param
     f1 = f1_score(y_test, y_pred, average='weighted')
     # confus_matrix = confusion_matrix(model, X_test, y_test)
     metrics = {'test_accuracy': accuracy
-            , 'test_f1_score': f1}
+        , 'test_f1_score': f1}
     print(metrics)
     return metrics
 
+
 if __name__ == '__main__':
-    GRAPH_VERSION = 5
-    DATASET_PATH = './data/code_blocks_regex_graph_v{}.csv'.format(GRAPH_VERSION)
+    GRAPH_VERSION = 6
+    DATASET_PATH = './data_mini/code_blocks_regex_graph_v{}.csv'.format(GRAPH_VERSION)
     MODEL_DIR = './models/svm_regex_graph_v{}.sav'.format(GRAPH_VERSION)
     TFIDF_DIR = './models/tfidf_svm_graph_v{}.pickle'.format(GRAPH_VERSION)
     CODE_COLUMN = 'code_block'
     TAGS_TO_PREDICT = ['import', 'data_import', 'data_export', 'preprocessing',
-                        'visualization', 'model', 'deep_learning_model', 'train', 'predict']
+                       'visualization', 'model', 'deep_learning_model', 'train', 'predict']
     SCRIPT_DIR = __file__
-    
+
     df, code_blocks = load_code_blocks(DATASET_PATH, CODE_COLUMN)
     nrows = df.shape[0]
     print("loaded")
     tfidf_params = {'min_df': 5
-            , 'max_df': 0.3
-            , 'smooth_idf': True}
-    SVM_params = {'C':100
-            , 'kernel':"linear"
-            , 'random_state':241}
+        , 'max_df': 0.3
+        , 'smooth_idf': True}
+    SVM_params = {'C': 100
+        , 'kernel': "linear"
+        , 'random_state': 241}
     data_meta = {'DATASET_PATH': DATASET_PATH
-                ,'nrows': nrows
-                ,'label': TAGS_TO_PREDICT
-                ,'model': MODEL_DIR
-                ,'script_dir': SCRIPT_DIR}
+        , 'nrows': nrows
+        , 'label': TAGS_TO_PREDICT
+        , 'model': MODEL_DIR
+        , 'script_dir': SCRIPT_DIR}
 
     with dagshub.dagshub_logger() as logger:
         print("evaluating..")
