460c67ec4b3470540289b2917bfee26bbdc81847
Alexander Levin <aslevin@edu.hse.ru>
2020-12-08 17:28:30 -0600

    added: evaluation option

--- regex.py
+++ regex.py
@@ -5,6 +5,8 @@ import argparse
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import f1_score, precision_score, recall_score
+import dagshub
 
 def tokens_search(df, tokens, new_column_name):
     df[new_column_name] = 0
@@ -22,12 +24,16 @@ def tokens_search(df, tokens, new_column_name):
 parser = argparse.ArgumentParser()
 parser.add_argument("GRAPH_VER", help="version of the graph you want regex to label your CSV with", type=int)
 parser.add_argument("DATASET_PATH", help="path to your input CSV", type=str)
+parser.add_argument("-eval", "--evaluation", help="evalute regex after creating", type=bool)
 args = parser.parse_args()
 
 GRAPH_VER = args.GRAPH_VER
 DATASET_PATH = args.DATASET_PATH
+evaluation = False
+if args.evaluation is not None:
+    evaluation = args.evaluation
 
-OUTPUT_DATASET_PATH = '{}_graph_v{}.csv'.format(DATASET_PATH[:-4], GRAPH_VER)
+OUTPUT_DATASET_PATH = '{}_regex_graph_v{}.csv'.format(DATASET_PATH[:-4], GRAPH_VER)
 CODE_COLUMN = 'code_block'
 GRAPH_DIR = './graph/graph_v{}.txt'.format(GRAPH_VER)
 
@@ -42,12 +48,19 @@ if __name__ == '__main__':
         graph = json.load(graph_file)
     print('opened graph')
 
+    vertices = []
     for i in range(0, len(graph)):
-        vertice = list(graph.keys())[i]
-        print('\n' + vertice)
-        tokens = graph[vertice]
-        df = tokens_search(df, tokens, vertice)
+        vertex = list(graph.keys())[i]
+        vertices.append(vertices)
+        print('\n' + vertex)
+        tokens = graph[vertex]
+        df = tokens_search(df, tokens, vertex)
     print('labelled')
 
     df.to_csv(OUTPUT_DATASET_PATH, index=False)
     print('saved and finished')
+    if evaluation:
+        VALIDATION_DATA_PATH = "./data/golden_884_set.csv"
+        TAGS = vertices
+        REGEX_TAGS = [el + '_regex_v{}'.format(GRAPH_VER) for el in TAGS]
+        regexed_data = pd.read_csv(VALIDATION_DATA_PATH)
