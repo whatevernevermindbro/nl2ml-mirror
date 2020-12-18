b96dc8cef1eae60d88cf50ed12f29dc6dc33954a
Alexander Levin <aslevin@edu.hse.ru>
2020-12-17 17:31:02 -0800

    added: methods get_graph_vertices, get_metrics

--- model_scripts/common/tools.py
+++ model_scripts/common/tools.py
@@ -1,13 +1,24 @@
 import pickle
+import json
 
 import pandas as pd
 import numpy as np
+import scipy
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import f1_score
 
 def load_data(DATASET_PATH):
     df = pd.read_csv(DATASET_PATH, encoding='utf-8', comment='#', sep=',')#, quoting=csv.QUOTE_NONE, error_bad_lines=False)#, sep=','
     df.dropna(axis=0, inplace=True)
     return df
+
+def get_graph_vertices(GRAPH_VER):
+    GRAPH_DIR = './graph/graph_v{}.txt'.format(GRAPH_VER)
+    with open(GRAPH_DIR, "r") as graph_file:
+        graph = json.load(graph_file)
+        vertices = list(graph.keys())
+    print('vertices parsed')
+    return vertices
 
 def tfidf_transform(corpus, tfidf_params, TFIDF_DIR):
     tfidf = pickle.load(open(TFIDF_DIR, 'rb'))
@@ -26,3 +37,17 @@ def mean_confidence_interval(data, confidence=0.95):
     m, se = np.mean(a), scipy.stats.sem(a)
     h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
     return m, m-h, m+h
+
+def get_metrics(X, y, TAGS_TO_PREDICT, MODEL_DIR):
+    clf = pickle.load(open(MODEL_DIR, 'rb'))
+    print("the model has been loaded")
+    y_pred = clf.predict(X)
+    print("predictions were calculated")
+    accuracy = clf.score(X, y)
+    f1 = f1_score(y_pred, y, average='weighted')
+    print(f'Mean Accuracy {round(accuracy*100, 2)}%')
+    print(f'F1-score {round(f1*100, 2)}%')
+    metrics = {'test_accuracy': accuracy
+               , 'test_f1_score': f1
+               }
+    return X, y, y_pred, metrics
