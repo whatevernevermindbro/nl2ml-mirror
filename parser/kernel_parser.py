da9eceaf3b12463c7764d704ac5927feb2c2b41b
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-02-07 00:56:30 +0300

    added progress bar

--- parser/kernel_parser.py
+++ parser/kernel_parser.py
@@ -2,11 +2,12 @@ import argparse
 from io import StringIO
 
 import pandas as pd
+from tqdm import tqdm
 
 from kaggle_scraping import KaggleWebDriver, extract_code_blocks
 
 
-KERNEL_FILENAME = "../data/kernels_list21.csv"
+KERNEL_FILENAME = "../also_data/kernels_list21.csv"
 refs = pd.read_csv(KERNEL_FILENAME)
 
 parser = argparse.ArgumentParser(description="Parse kaggle notebooks")
@@ -20,6 +21,8 @@ ERRORS_FILENAME = "errors_blocks.csv"
 webdriver = KaggleWebDriver()
 webdriver.load()
 
+pbar = tqdm(total=(refs.shape[0] // 3))
+
 with open(CODEBLOCK_FILENAME, mode='w') as f:
     with open(ERRORS_FILENAME, mode='w') as e:
         for i in range(int(args["--process_id"]), refs.shape[0], 3):
@@ -28,5 +31,7 @@ with open(CODEBLOCK_FILENAME, mode='w') as f:
                 print(buf.getvalue(), file=f)
             except Exception as exept:
                 e.write(refs.ref[i] + "\n")
+            pbar.update(1)
 
 webdriver.close()
+pbar.close()
