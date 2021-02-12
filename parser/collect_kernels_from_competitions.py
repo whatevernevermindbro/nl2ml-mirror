e6bd8abadf8c608b30eb2e784087c715f1ec4eeb
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-02-12 21:53:05 +0300

    upd:parsing metric names

--- parser/collect_kernels_from_competitions.py
+++ parser/collect_kernels_from_competitions.py
@@ -3,6 +3,8 @@ import subprocess
 
 import pandas as pd
 
+COMPETITION_REFS_FILE = "competitions_ref.csv"
+comp_refs = None
 
 for i in range(11):
     result = subprocess.run(
@@ -17,5 +19,13 @@ for i in range(11):
 
     competitions = pd.read_csv(StringIO(result.stdout), header=0)
 
+    if i == 0:
+        comp_refs = competitions
+    else:
+        comp_refs = pd.concat([comp_refs, competitions])
+
     for ref in competitions.ref:
         subprocess.run(["./competition_kernels.sh", ref])
+
+comp_refs = comp_refs.drop(columns=["userHasEntered", "deadline", "category", "reward", "teamCount"])
+comp_refs.to_csv(COMPETITION_REFS_FILE)
