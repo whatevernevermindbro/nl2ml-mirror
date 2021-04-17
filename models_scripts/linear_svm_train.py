9085fd7a6635923e1fcf360b126cebef3b35f740
Polina Guseva <p.guseva00@gmail.com>
2021-04-17 15:26:33 +0300

    upd: added hyperparam search

--- models_scripts/linear_svm_train.py
+++ models_scripts/linear_svm_train.py
@@ -6,61 +6,13 @@ import os
 
 import pandas as pd
 import numpy as np
+import optuna
 from sklearn.metrics import accuracy_score, f1_score
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.model_selection import KFold
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import BaggingClassifier
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.multioutput import MultiOutputRegressor
 import dagshub
 
 from common.tools import *
-
-
-EXPERIMENT_DATA_PATH = ".."
-CODE_COLUMN = "code_block"
-TARGET_COLUMN = "graph_vertex_id"
-
-
-def SVM_multioutput_evaluate(df, tfidf_params, TFIDF_DIR, svm_params):
-    code_blocks_tfidf = tfidf_fit_transform(df[CODE_COLUMN], tfidf_params, TFIDF_DIR)
-    print(df.columns)
-    X, y = code_blocks_tfidf, df[TARGET_COLUMN].values
-
-    clf = LinearSVC(**svm_params)
-    print("Model params:", clf.get_params())
-
-    n_folds = 20
-    kf = KFold(n_splits=n_folds)
-
-    f1s = []
-    accuracies = []
-    for i, (train_index, test_index) in enumerate(kf.split(X)):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-
-        print("{:2}/{:2} training".format(i, n_folds))
-        clf.fit(X_train, y_train)
-
-        print("{:2}/{:2} predicting on test".format(i, n_folds))
-        y_pred = clf.predict(X_test)
-        accuracies.append(accuracy_score(y_test, y_pred))
-        f1s.append(f1_score(y_test, y_pred, average='weighted'))
-
-    f1s = np.array(f1s)
-    accuracies = np.array(accuracies)
-
-    metrics = {'test_accuracy': accuracies.mean(), 'test_f1_score': f1s.mean()}
-    print(metrics)
-
-    print("saving model")
-
-    clf.fit(X, y)
-    pickle.dump(clf, open(MODEL_DIR, "wb"))
-
-    return metrics
 
 
 parser = argparse.ArgumentParser()
@@ -76,6 +28,104 @@ TFIDF_DIR = "../models/tfidf_svm_graph_v{}.pickle".format(GRAPH_VER)
 
 TAGS_TO_PREDICT = get_graph_vertices(GRAPH_VER)
 
+EXPERIMENT_DATA_PATH = ".."
+CODE_COLUMN = "code_block"
+TARGET_COLUMN = "graph_vertex_id"
+
+RANDOM_STATE = 42
+N_TRIALS = 50
+HYPERPARAM_SPACE = {
+    "svm_c": (1e-3, 1e3),
+    "tfidf_min_df": (1, 50),
+    "tfidf_max_df": (0.2, 1.0),
+}
+
+
+def cross_val_scores(kf, clf, X, y):
+    f1s = []
+    accuracies = []
+    for i, (train_index, test_index) in enumerate(kf.split(X)):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        clf.fit(X_train, y_train)
+
+        y_pred = clf.predict(X_test)
+        f1s.append(f1_score(y_test, y_pred, average='weighted'))
+        accuracies.append(accuracy_score(y_test, y_pred))
+
+    f1s = np.array(f1s)
+    accuracies = np.array(accuracies)
+    return f1s.mean(), accuracies.mean()
+
+
+class Objective:
+    def __init__(self, df, kfold_params, svm_c, tfidf_min_df, tfidf_max_df):
+        self.kf = KFold(**kfold_params)
+        self.c_range = svm_c
+        self.min_df_range = tfidf_min_df
+        self.max_df_range = tfidf_max_df
+        self.df = df
+
+    def __call__(self, trial):
+        tfidf_params = {
+            "min_df": trial.suggest_int("tfidf__min_df", *self.min_df_range),
+            "max_df": trial.suggest_loguniform("tfidf__max_df", *self.max_df_range),
+            "smooth_idf": True,
+        }
+        code_blocks_tfidf = tfidf_fit_transform(self.df[CODE_COLUMN], tfidf_params)
+        X, y = code_blocks_tfidf, self.df[TARGET_COLUMN].values
+
+        svm_params = {
+            "C": trial.suggest_loguniform("svm__C", *self.c_range),
+            "random_state": RANDOM_STATE,
+        }
+        clf = LinearSVC(**svm_params)
+
+        f1_mean, _ = cross_val_scores(self.kf, clf, X, y)
+        return f1_mean
+
+
+def select_hyperparams(df, kfold_params, tfidf_path, model_path):
+    """
+    Uses optuna to find hyperparams that maximize F1 score
+    :param df: labelled dataset
+    :param kfold_params: parameters for sklearn's KFold
+    :param tfidf_dir: where to save trained tf-idf
+    :return: dict with parameters and metrics
+    """
+    study = optuna.create_study(direction="maximize")
+    objective = Objective(df, kfold_params, **HYPERPARAM_SPACE)
+
+    study.optimize(objective, n_trials=N_TRIALS)
+
+    best_tfidf_params = {
+        "smooth_idf": True,
+    }
+    best_svm_params = {
+        "random_state": RANDOM_STATE,
+    }
+    for key, value in study.best_params.items():
+        model_name, param_name = key.split("__")
+        if model_name == "tfidf":
+            best_tfidf_params[param_name] = value
+        elif model_name == "svm":
+            best_svm_params[param_name] = value
+
+    code_blocks_tfidf = tfidf_fit_transform(df[CODE_COLUMN], best_tfidf_params, tfidf_path)
+    X, y = code_blocks_tfidf, df[TARGET_COLUMN].values
+    clf = LinearSVC(**best_svm_params)
+
+    f1_mean, accuracy_mean = cross_val_scores(objective.kf, clf, X, y)
+
+    clf.fit(X, y)
+    pickle.dump(clf, open(model_path, "wb"))
+
+    metrics = dict(test_f1=f1_mean, test_accuracy=accuracy_mean)
+
+    return best_tfidf_params, best_svm_params, metrics
+
+
 if __name__ == "__main__":
     df = load_data(DATASET_PATH)
 
@@ -83,18 +133,10 @@ if __name__ == "__main__":
     nrows = df.shape[0]
     print("loaded")
 
-    tfidf_params = {
-        'min_df': 5,
-        'max_df': 0.3,
-        'smooth_idf': True,
-    }
     kfold_params = {
-        "random_state": 42,
+        "n_splits": 10,
+        "random_state": RANDOM_STATE,
         "shuffle": True,
-    }
-    svm_params = {
-        'C': 5,
-        'random_state': 241,
     }
     data_meta = {
         'DATASET_PATH': DATASET_PATH,
@@ -107,9 +149,9 @@ if __name__ == "__main__":
     metrics_path = os.path.join(EXPERIMENT_DATA_PATH, "metrics.csv")
     params_path = os.path.join(EXPERIMENT_DATA_PATH, "params.yml")
     with dagshub.dagshub_logger(metrics_path=metrics_path, hparams_path=params_path) as logger:
-        print("evaluating..")
-        metrics = SVM_multioutput_evaluate(df, tfidf_params, TFIDF_DIR, svm_params)
-        print("saving the results..")
+        print("selecting hyperparameters")
+        tfidf_params, svm_params, metrics = select_hyperparams(df, kfold_params, TFIDF_DIR, MODEL_DIR)
+        print("logging the results")
         logger.log_hyperparams({"data": data_meta})
         logger.log_hyperparams({"tfidf": tfidf_params})
         logger.log_hyperparams({"model": svm_params})
