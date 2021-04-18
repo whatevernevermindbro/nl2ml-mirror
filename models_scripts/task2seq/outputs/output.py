fb3e1ab3981a0e847ff58a0eb886d28fc7e4156c
Alexander Levin <sanchezable@yandex.ru>
2021-04-18 16:06:54 +0300

    added: format of the generative model output

---
+++ models_scripts/task2seq/outputs/output.py
@@ -0,0 +1,82 @@
+#@ import_modules 
+
+#@ load_from_csv 
+
+#@ show_table 
+
+#@ show_table_attributes 
+
+#@ load_from_csv 
+
+#@ show_table 
+
+#@ show_table_attributes 
+
+#@ count_duplicates 
+
+#@ create_dataframe 
+
+#@ count_missing_values 
+
+#@ correct_missing_values 
+
+#@ count_data_types 
+
+#@ correct_missing_values 
+
+#@ count_missing_values 
+
+#@ correct_missing_values 
+
+#@ missing_values 
+
+#@ count_missing_values 
+
+#@ correct_missing_values 
+
+#@ count_data_types 
+
+#@ correct_missing_values 
+
+#@ count_missing_values 
+
+#@ correct_missing_values 
+
+#@ missing_values 
+
+#@ show_table_attributes 
+
+#@ concatenate 
+
+#@ data_types_conversions 
+
+#@ show_table 
+
+#@ count_missing_values 
+
+#@ filter 
+
+#@ show_table 
+
+#@ filter 
+
+#@ distribution 
+
+#@ normalization 
+
+#@ split 
+
+#@ normalization 
+
+#@ choose_model_class 
+
+#@ find_best_model_class 
+
+#@ choose_model_class 
+
+#@ predict 
+
+#@ save_to_csv 
+
+#@ distribution 
+
