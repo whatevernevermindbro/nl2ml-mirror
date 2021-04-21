3622bbe4a0860c6f9d81d497dcab2c9b6eb10e2a
dsapoggit <d.sapogmail@yandex.ru>
2021-04-21 16:13:00 +0300

    method parse 2

--- parser/auto_features/methods_parse/methods_parse.py
+++ parser/auto_features/methods_parse/methods_parse.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
 import numpy as np
 import pandas as pd
 import ast
-
+import traceback
 
 
 def UsedMethods2(code):
   try:
-    p = ast.parse(code)
+    p = ast.parse(code.strip('`'))
     names = sorted({node.attr for node in ast.walk(p) if isinstance(node, ast.Attribute)}) #but here we also have node.id for example
     result = list()
     for instance in names:
@@ -26,43 +32,57 @@ def UsedMethods(code):
     p = ast.parse(code.strip('`'))
     names = sorted({node.attr for node in ast.walk(p) if isinstance(node, ast.Attribute)})
     return names
-  except Exception as e:
-    # print(e)
-    return list()
+  except:
+    names = list()
+    dot_pos = code.find('.')
+    
+    while dot_pos != -1:
+        dot_pos += 1
+        name = ""
+        while (dot_pos < len(code)) and (code[dot_pos].isalpha() or code[dot_pos].isdigit() or code[dot_pos] in {'(', '.'}):
+            if code[dot_pos] in {'(', '.'}:
+                names.append(name)
+                break
+            name += code[dot_pos]
+            dot_pos += 1
+
+        dot_pos = code.find('.', dot_pos)  
+    
+    return names
     
 
 #load dataframe    
-# filename = "11.csv" #enter the name of a .csv file
-# df = pd.read_csv(filename)
+filename = "markup_data.csv" #enter the name of a .csv file
+df = pd.read_csv(filename)
 
-
-# #chop off the head 
-# df.columns = df.iloc[0,:]
-# df = df.drop(index=0)
 
 #fill methods for each block
 
-# df['python_methods'] = df['code_block'].apply(UsedMethods)
-
+df['python_methods'] = df['code_block'].apply(UsedMethods)
+###
 
 #fill methods for nearrby blocks
-def shift_methods(df):
-    df['python_methods_p1'] = np.NaN
-    df['python_methods_p2'] = np.NaN
-    df['python_methods_p3'] = np.NaN
-    df['python_methods_m1'] = np.NaN
-    df['python_methods_m2'] = np.NaN
-    df['python_methods_m3'] = np.NaN
+df['python_methods_p1'] = np.NaN
+df['python_methods_p2'] = np.NaN
+df['python_methods_p3'] = np.NaN
+df['python_methods_m1'] = np.NaN
+df['python_methods_m2'] = np.NaN
+df['python_methods_m3'] = np.NaN
 
-    df['python_methods_m1'][1:] = df['python_methods'][:-1]
-    df['python_methods_m2'][2:] = df['python_methods'][:-2]
-    df['python_methods_m3'][3:] = df['python_methods'][:-3]
-    df['python_methods_p1'][:-1] = df['python_methods'][1:]
-    df['python_methods_p2'][:-2] = df['python_methods'][2:]
-    df['python_methods_p3'][:-3] = df['python_methods'][3:]
-    return df
+df['python_methods_m1'][1:] = df['python_methods'][:-1]
+df['python_methods_m2'][2:] = df['python_methods'][:-2]
+df['python_methods_m3'][3:] = df['python_methods'][:-3]
+df['python_methods_p1'][:-1] = df['python_methods'][1:]
+df['python_methods_p2'][:-2] = df['python_methods'][2:]
+df['python_methods_p3'][:-3] = df['python_methods'][3:]
 
 
-# #export dataframe
-# df.to_csv(filename[:-4] + "python_methods.csv", index=False)
+#export dataframe
+df.to_csv(filename[:-4] + "_python_methods.csv", index=False)
+
+
+# In[ ]:
+
+
+
 
