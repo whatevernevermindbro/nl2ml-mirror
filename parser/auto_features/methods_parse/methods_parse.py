cfd9c0e2bcdbd4c70fcc90c6164018bf8f1707af
Daria Sapozhnikova <d.sapogmail@yandex.ru>
2021-04-21 13:48:21 +0000

    Update methods_parse.py

--- parser/auto_features/methods_parse/methods_parse.py
+++ parser/auto_features/methods_parse/methods_parse.py
@@ -52,37 +52,38 @@ def UsedMethods(code):
     
 
 #load dataframe    
-filename = "markup_data.csv" #enter the name of a .csv file
-df = pd.read_csv(filename)
+# filename = "11.csv" #enter the name of a .csv file
+# df = pd.read_csv(filename)
 
+
+# #chop off the head 
+# df.columns = df.iloc[0,:]
+# df = df.drop(index=0)
 
 #fill methods for each block
 
-df['python_methods'] = df['code_block'].apply(UsedMethods)
-###
+# df['python_methods'] = df['code_block'].apply(UsedMethods)
+
 
 #fill methods for nearrby blocks
-df['python_methods_p1'] = np.NaN
-df['python_methods_p2'] = np.NaN
-df['python_methods_p3'] = np.NaN
-df['python_methods_m1'] = np.NaN
-df['python_methods_m2'] = np.NaN
-df['python_methods_m3'] = np.NaN
+def shift_methods(df):
+    df['python_methods_p1'] = np.NaN
+    df['python_methods_p2'] = np.NaN
+    df['python_methods_p3'] = np.NaN
+    df['python_methods_m1'] = np.NaN
+    df['python_methods_m2'] = np.NaN
+    df['python_methods_m3'] = np.NaN
 
-df['python_methods_m1'][1:] = df['python_methods'][:-1]
-df['python_methods_m2'][2:] = df['python_methods'][:-2]
-df['python_methods_m3'][3:] = df['python_methods'][:-3]
-df['python_methods_p1'][:-1] = df['python_methods'][1:]
-df['python_methods_p2'][:-2] = df['python_methods'][2:]
-df['python_methods_p3'][:-3] = df['python_methods'][3:]
-
-
-#export dataframe
-df.to_csv(filename[:-4] + "_python_methods.csv", index=False)
+    df['python_methods_m1'][1:] = df['python_methods'][:-1]
+    df['python_methods_m2'][2:] = df['python_methods'][:-2]
+    df['python_methods_m3'][3:] = df['python_methods'][:-3]
+    df['python_methods_p1'][:-1] = df['python_methods'][1:]
+    df['python_methods_p2'][:-2] = df['python_methods'][2:]
+    df['python_methods_p3'][:-3] = df['python_methods'][3:]
+    return df
 
 
-# In[ ]:
-
-
+# #export dataframe
+# df.to_csv(filename[:-4] + "python_methods.csv", index=False)
 
 
