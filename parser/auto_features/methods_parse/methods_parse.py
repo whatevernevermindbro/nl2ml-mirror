d5a03fca40fb632fb5aef4c0f95e338e57cc77ab
Alexander Levin <sanchezable@yandex.ru>
2021-04-21 14:48:51 +0300

    added: print Exception

--- parser/auto_features/methods_parse/methods_parse.py
+++ parser/auto_features/methods_parse/methods_parse.py
@@ -26,40 +26,43 @@ def UsedMethods(code):
     p = ast.parse(code)
     names = sorted({node.attr for node in ast.walk(p) if isinstance(node, ast.Attribute)})
     return names
-  except:
+  except Exception as e:
+    print(e)
     return list()
     
 
 #load dataframe    
-filename = "11.csv" #enter the name of a .csv file
-df = pd.read_csv(filename)
+# filename = "11.csv" #enter the name of a .csv file
+# df = pd.read_csv(filename)
 
 
-#chop off the head 
-df.columns = df.iloc[0,:]
-df = df.drop(index=0)
+# #chop off the head 
+# df.columns = df.iloc[0,:]
+# df = df.drop(index=0)
 
 #fill methods for each block
 
-df['python_methods'] = df['code_block'].apply(UsedMethods)
+# df['python_methods'] = df['code_block'].apply(UsedMethods)
 
 
 #fill methods for nearrby blocks
-df['python_methods_p1'] = np.NaN
-df['python_methods_p2'] = np.NaN
-df['python_methods_p3'] = np.NaN
-df['python_methods_m1'] = np.NaN
-df['python_methods_m2'] = np.NaN
-df['python_methods_m3'] = np.NaN
+def shift_methods(df):
+    df['python_methods_p1'] = np.NaN
+    df['python_methods_p2'] = np.NaN
+    df['python_methods_p3'] = np.NaN
+    df['python_methods_m1'] = np.NaN
+    df['python_methods_m2'] = np.NaN
+    df['python_methods_m3'] = np.NaN
 
-df['python_methods_m1'][1:] = df['python_methods'][:-1]
-df['python_methods_m2'][2:] = df['python_methods'][:-2]
-df['python_methods_m3'][3:] = df['python_methods'][:-3]
-df['python_methods_p1'][:-1] = df['python_methods'][1:]
-df['python_methods_p2'][:-2] = df['python_methods'][2:]
-df['python_methods_p3'][:-3] = df['python_methods'][3:]
+    df['python_methods_m1'][1:] = df['python_methods'][:-1]
+    df['python_methods_m2'][2:] = df['python_methods'][:-2]
+    df['python_methods_m3'][3:] = df['python_methods'][:-3]
+    df['python_methods_p1'][:-1] = df['python_methods'][1:]
+    df['python_methods_p2'][:-2] = df['python_methods'][2:]
+    df['python_methods_p3'][:-3] = df['python_methods'][3:]
+    return df
 
 
-#export dataframe
-df.to_csv(filename[:-4] + "python_methods.csv", index=False)
+# #export dataframe
+# df.to_csv(filename[:-4] + "python_methods.csv", index=False)
 
